5e7b1991646a1c7379d6fbecc04f4a6a39d58c57
Khue Lee <khuelee55@gmail.com>
2023-02-16 20:18:47 -0600

    added query text

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -6,6 +6,19 @@ router.get('/:id', (req, res) => {
   // Add query to get all genres
   console.log(req.params.id);
   // res.sendStatus(500);
+  const id = req.params.id;
+  const queryText = `SELECT "genres"."name" FROM "movies"
+    JOIN "movies_genres" ON "movies_genres"."movie_id" = "movies"."id"
+    JOIN "genres" ON "genres"."id" = "movies_genres"."genre_id"
+    WHERE "movies"."id" = ${id};`;
+  pool.query(queryText)
+  .then((response) => {
+    console.log(response);
+    res.send(response.rows);
+  })
+  .catch((err) => {
+    res.sendStatus(500);
+  });
 });
 
 module.exports = router;
